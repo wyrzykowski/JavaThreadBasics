847926f510f05fa0045ebea387a985c8f3f5e760
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-11-21 21:20:07 +0100

    Add mutli threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] arg){
 
         ThreadClass thread1 = new ThreadClass();
+        ThreadClass thread2 = new ThreadClass();
+        ThreadClass thread3 = new ThreadClass();
 
         thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
