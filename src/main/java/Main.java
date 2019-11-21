2858840c91a6bd11c965cb041daea417cb9c2494
wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-11-21 22:15:07 +0100

    Removed comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,22 +9,22 @@ public class Main {
     public static void main(String[] arg){
 
         //Approach I - extends Threads class
-//        ThreadClass thread1 = new ThreadClass();
-//        ThreadClass thread2 = new ThreadClass();
-//        ThreadClass thread3 = new ThreadClass();
+        ThreadClass thread1 = new ThreadClass();
+        ThreadClass thread2 = new ThreadClass();
+        ThreadClass thread3 = new ThreadClass();
 
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
 
         //Approach II - implements runnable interface
 
-//        Thread threadRunnable1 = new Thread(new ThreadClassRunnable());
-//        Thread threadRunnable2 = new Thread(new ThreadClassRunnable());
-//
-//        threadRunnable1.start();
-//        threadRunnable2.start();
+        Thread threadRunnable1 = new Thread(new ThreadClassRunnable());
+        Thread threadRunnable2 = new Thread(new ThreadClassRunnable());
+
+        threadRunnable1.start();
+        threadRunnable2.start();
 
         //Synchronizing threads methods
 
