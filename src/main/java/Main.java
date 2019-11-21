6d4e10d34380517f0b393fff4208b19264c186c6
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-11-21 21:41:39 +0100

    Create thread by implementing Runnable interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,24 @@ public class Main {
 
     public static void main(String[] arg){
 
-        ThreadClass thread1 = new ThreadClass();
-        ThreadClass thread2 = new ThreadClass();
-        ThreadClass thread3 = new ThreadClass();
+        //Approach I - extends Threads class
+//        ThreadClass thread1 = new ThreadClass();
+//        ThreadClass thread2 = new ThreadClass();
+//        ThreadClass thread3 = new ThreadClass();
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+
+
+        //Approach II - implements runnable interface
+
+        Thread threadRunnable1 = new Thread(new ThreadClassRunnable());
+        Thread threadRunnable2 = new Thread(new ThreadClassRunnable());
+
+
+
+        threadRunnable1.start();
+        threadRunnable2.start();
     }
 }
